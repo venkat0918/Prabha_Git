2a7cfcaa7c562c97515390f3e00244de32749b57
cr7pr9 <prabhakarreddy2502@gmail.com>
2020-09-19 12:19:42 +0530

    Third Commit

--- Code_Prctise/salesforce.py
+++
@@ -1,6 +0,0 @@
-from simple_salesforce import Salesforce
-import json
-sf = Salesforce(username='prabha.boddapati4@gmail.com', password='Prabha@0409', security_token='52INtn8s2AstLy8nNpCZTtXe')
-data = sf.query("Select Name,Id  from Account where Id ='0012w00000LzKcUAAV'")
-put_dict=json.loads(json.dumps(data))
-print(put_dict['records'][0]['Name'])
